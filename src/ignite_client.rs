0cc3e73fdb65450b55d57654a395f2280c73164b
Igor Sapego <igorsapg@gmail.com>
2018-11-21 10:01:40 +0300

    Implemented IgniteClient::start()

--- src/ignite_client.rs
+++ src/ignite_client.rs
@@ -1,3 +1,13 @@
+#[derive(Debug)]
+struct IgniteClient {
+}
+
+impl IgniteClient {
+    /// Start Ignite client with default configuration.
+    pub fn start() -> Result<IgniteClient, String> {
+        Ok(IgniteClient{})
+    }
+}
 
 #[test]
 fn ignite_client_new_default() {
