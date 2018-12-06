0a6c2ea5e943189f56d4d9606b6ba93201ad8a23
Igor Sapego <igorsapg@gmail.com>
2018-12-06 23:40:53 +0300

    Added new test

--- tests/ignite_client_test.rs
+++ tests/ignite_client_test.rs
@@ -1,6 +1,16 @@
+extern crate rand;
 extern crate ignite_rust;
 
 use ignite_rust::*;
+use rand::{thread_rng, Rng};
+use rand::distributions::Alphanumeric;
+
+fn make_unique_name() -> String {
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(64)
+        .collect()
+}
 
 #[test]
 fn ignite_client_start_default() {
@@ -13,4 +23,17 @@ fn ignite_client_start_with_config() {
     cfg.set_endpoints("127.0.0.1:10800");
 
     IgniteClient::start(cfg).unwrap();
+}
+
+#[test]
+fn ignite_client_create_cache() {
+    let mut cfg = IgniteConfiguration::new();
+    cfg.set_endpoints("127.0.0.1:10800");
+
+    let mut client = IgniteClient::start(cfg).unwrap();
+
+    let cache_name = make_unique_name();
+
+    client.create_cache(cache_name).expect("Success expected");
+    client.create_cache(cache_name).expect_err("Error expected: cache with the name should be created already");
 }
