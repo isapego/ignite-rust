8ed03fb85d74f308ba1f806fa8c9d20d33237b76
Igor Sapego <igorsapg@gmail.com>
2018-12-11 11:34:49 +0300

    Added test

--- src/end_point.rs
+++ src/end_point.rs
@@ -30,13 +30,17 @@ impl EndPoint {
             None => return Err("Parsing error: Host can not be an empty string".into()),
         };
 
+        if host.is_empty() {
+            return Err("Parsing error: Host can not be an empty string".into());
+        }
+
         let range = match iter.next() {
             Some(r) => r,
             None => return Ok(EndPoint::new(host, 0, 0)),
         };
 
         if iter.next().is_some() {
-            return Err("Parsing error: Unexpected number of semicolons in endpoint".into());
+            return Err("Parsing error: Unexpected number of semicolons ':' in endpoint".into());
         };
 
         let mut range_iter = range.trim().split("..");
@@ -56,6 +60,10 @@ impl EndPoint {
             }
         };
 
+        if port_begin == 0 {
+            return Err("Parsing error: TCP port can not be zero".into());
+        }
+
         let port_end_s = match range_iter.next() {
             Some(p) => p,
             None => return Ok(EndPoint::new(host, port_begin, 0)),
@@ -71,6 +79,18 @@ impl EndPoint {
             }
         };
 
+        if port_begin > port_end {
+            return Err(
+                "Parsing error: beginning of the port range can not be bigger than the end".into(),
+            );
+        }
+
+        if range_iter.next().is_some() {
+            return Err(
+                "Parsing error: Unexpected number of range separators '..' in endpoint".into(),
+            );
+        };
+
         Ok(EndPoint::new(host, port_begin, port_end))
     }
 }
@@ -82,3 +102,63 @@ impl EndPoint {
 
 //     }).collect()
 // }
+
+#[test]
+fn end_point_from_string() {
+    EndPoint::from_string("127.0.0.1").unwrap();
+    EndPoint::from_string("127.0.0.1:42").unwrap();
+    EndPoint::from_string("127.0.0.1:1..2").unwrap();
+    EndPoint::from_string("example.com").unwrap();
+    EndPoint::from_string("example.com:42").unwrap();
+    EndPoint::from_string("example.com:42..42").unwrap();
+    EndPoint::from_string("example.com:42..101").unwrap();
+    EndPoint::from_string("example.com:54..54").unwrap();
+
+    EndPoint::from_string("").unwrap_err();
+    EndPoint::from_string(":").unwrap_err();
+    EndPoint::from_string(":0").unwrap_err();
+    EndPoint::from_string(":0..12").unwrap_err();
+    EndPoint::from_string(":12").unwrap_err();
+    EndPoint::from_string(":41..42").unwrap_err();
+    EndPoint::from_string(":42..41").unwrap_err();
+    EndPoint::from_string(":..").unwrap_err();
+    EndPoint::from_string(":42..").unwrap_err();
+    EndPoint::from_string(":..42").unwrap_err();
+    EndPoint::from_string(":..42..").unwrap_err();
+    EndPoint::from_string(":12..42..").unwrap_err();
+    EndPoint::from_string(":..12..42").unwrap_err();
+    EndPoint::from_string(":..12..42..").unwrap_err();
+    EndPoint::from_string(":47293875987").unwrap_err();
+    EndPoint::from_string(":12234546..42341245436").unwrap_err();
+    EndPoint::from_string(":1..47293875987").unwrap_err();
+
+    EndPoint::from_string("127.0.0.1:").unwrap_err();
+    EndPoint::from_string("127.0.0.1:0").unwrap_err();
+    EndPoint::from_string("127.0.0.1:0..12").unwrap_err();
+    EndPoint::from_string("127.0.0.1:42..41").unwrap_err();
+    EndPoint::from_string("127.0.0.1:..").unwrap_err();
+    EndPoint::from_string("127.0.0.1:42..").unwrap_err();
+    EndPoint::from_string("127.0.0.1:..42").unwrap_err();
+    EndPoint::from_string("127.0.0.1:..42..").unwrap_err();
+    EndPoint::from_string("127.0.0.1:12..42..").unwrap_err();
+    EndPoint::from_string("127.0.0.1:..12..42").unwrap_err();
+    EndPoint::from_string("127.0.0.1:..12..42..").unwrap_err();
+    EndPoint::from_string("127.0.0.1:47293875987").unwrap_err();
+    EndPoint::from_string("127.0.0.1:12234546..42341245436").unwrap_err();
+    EndPoint::from_string("127.0.0.1:1..47293875987").unwrap_err();
+
+    EndPoint::from_string("example.com:").unwrap_err();
+    EndPoint::from_string("example.com:0").unwrap_err();
+    EndPoint::from_string("example.com:0..12").unwrap_err();
+    EndPoint::from_string("example.com:42..41").unwrap_err();
+    EndPoint::from_string("example.com:..").unwrap_err();
+    EndPoint::from_string("example.com:42..").unwrap_err();
+    EndPoint::from_string("example.com:..42").unwrap_err();
+    EndPoint::from_string("example.com:..42..").unwrap_err();
+    EndPoint::from_string("example.com:12..42..").unwrap_err();
+    EndPoint::from_string("example.com:..12..42").unwrap_err();
+    EndPoint::from_string("example.com:..12..42..").unwrap_err();
+    EndPoint::from_string("example.com:47293875987").unwrap_err();
+    EndPoint::from_string("example.com:12234546..42341245436").unwrap_err();
+    EndPoint::from_string("example.com:1..47293875987").unwrap_err();
+}
