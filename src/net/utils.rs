7971d7d3bd74e1797c85c212b415e2c9ce544ef6
Igor Sapego <igorsapg@gmail.com>
2018-12-13 02:58:15 +0300

    Added test

--- src/net/utils.rs
+++ src/net/utils.rs
@@ -10,3 +10,31 @@ pub fn parse_endpoints(end_points: &str) -> IgniteResult<Vec<EndPoint>> {
     }
     Ok(res)
 }
+
+#[test]
+fn test_parse_endpoints() {
+    parse_endpoints("127.0.0.1").unwrap();
+    parse_endpoints("example.com").unwrap();
+    parse_endpoints("localhost").unwrap();
+    parse_endpoints("127.0.0.1:10800").unwrap();
+    parse_endpoints("127.0.0.1:10800..10810").unwrap();
+    parse_endpoints("127.0.0.1,example.com").unwrap();
+    parse_endpoints("127.0.0.1, example.com").unwrap();
+    parse_endpoints("127.0.0.1, example.com ").unwrap();
+    parse_endpoints("127.0.0.1,example.com,localhost").unwrap();
+    parse_endpoints("127.0.0.1:12,example.com,localhost").unwrap();
+    parse_endpoints("127.0.0.1,example.com:12,localhost").unwrap();
+    parse_endpoints("127.0.0.1,example.com,localhost:12").unwrap();
+    parse_endpoints("127.0.0.1:12,example.com:12,localhost").unwrap();
+    parse_endpoints("127.0.0.1:12,example.com,localhost:12").unwrap();
+    parse_endpoints("127.0.0.1:12,example.com:12,localhost:12").unwrap();
+    parse_endpoints("127.0.0.1:12..42,example.com:12,localhost:12").unwrap();
+    parse_endpoints("127.0.0.1:12..42,example.com:12..42,localhost:12").unwrap();
+    parse_endpoints("127.0.0.1:12..42,example.com:12,localhost:12..42").unwrap();
+    parse_endpoints("127.0.0.1:12..42,example.com:12..42,localhost:12..42").unwrap();
+    parse_endpoints("127.0.0.1:12..42,example.com,localhost:12").unwrap();
+
+    parse_endpoints("127.0.0.1:12..42,,localhost:12").unwrap_err();
+    parse_endpoints("127.0.0.1:12..42,localhost:12,").unwrap_err();
+    parse_endpoints(",127.0.0.1:12..42,localhost:12").unwrap_err();
+}
