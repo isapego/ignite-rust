715c4690a2a6c3541e1f59af5d9a583278d46520
Igor Sapego <igorsapg@gmail.com>
2019-02-09 15:26:50 +0300

    Implemented round_to_pow2_u32

--- src/protocol/utils.rs
+++ src/protocol/utils.rs
@@ -15,6 +15,23 @@ pub fn deserialize_readable<T: Readable<Item=T>>(data: &[u8]) -> T {
     T::read(&stream)
 }
 
+/// Calculate the value fast which is the power of two and is greater or equals to the provided
+/// value. See https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2 for details.
+pub fn round_to_pow2_u32(val: u32) -> u32 {
+    assert_ne!(val, 0, "Value can not be zero");
+    assert!(val <= (1 << 31), "Value can not be bigger than 2^31");
+
+    let mut v = val;
+
+    v -= 1;
+    v |= v >> 1;
+    v |= v >> 2;
+    v |= v >> 4;
+    v |= v >> 8;
+    v |= v >> 16;
+    v + 1
+}
+
 #[test]
 fn test_deserialize_i32_1() {
     let data = [1u8, 0, 0, 0];
@@ -31,4 +48,38 @@ fn test_deserialize_i32_x11378caa() {
     let res = deserialize_i32(&data);
 
     assert_eq!(res, 0x11378CAA);
+}
+
+#[test]
+fn test_round_to_pow2_exact() {
+    assert_eq!(1, round_to_pow2_u32(1));
+    assert_eq!(2, round_to_pow2_u32(2));
+    assert_eq!(4, round_to_pow2_u32(4));
+    assert_eq!(8, round_to_pow2_u32(8));
+    assert_eq!(16, round_to_pow2_u32(16));
+    assert_eq!(32, round_to_pow2_u32(32));
+    assert_eq!(64, round_to_pow2_u32(64));
+    assert_eq!(256, round_to_pow2_u32(256));
+    assert_eq!(1024, round_to_pow2_u32(1024));
+
+    for i in 0..32 {
+        assert_eq!(1 << i, round_to_pow2_u32(1 << i));
+    }
+}
+
+#[test]
+fn test_round_to_pow2_random() {
+    assert_eq!(4, round_to_pow2_u32(3));
+    assert_eq!(8, round_to_pow2_u32(5));
+    assert_eq!(16, round_to_pow2_u32(12));
+    assert_eq!(32, round_to_pow2_u32(30));
+    assert_eq!(64, round_to_pow2_u32(53));
+    assert_eq!(256, round_to_pow2_u32(211));
+    assert_eq!(1024, round_to_pow2_u32(618));
+
+    assert_eq!(1 << 31, round_to_pow2_u32(::std::i32::MAX as u32));
+
+    for i in 2..32 {
+        assert_eq!(1 << i, round_to_pow2_u32((1 << i) - 1));
+    }
 }
